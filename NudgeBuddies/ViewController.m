217972fd71eed218705a4ff76bb5b2933131c765
Xian Lee <xianlee@xians-mac.local>
2015-12-03 04:08:19 +0800

    Initial Commit

---
+++ NudgeBuddies/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NudgeBuddies
+//
+//  Created by Xian Lee on 3/12/2015.
+//  Copyright © 2015 Blue Silver. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
